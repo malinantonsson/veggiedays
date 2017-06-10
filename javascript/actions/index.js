48327a39fd700150543b03c7350129eb946f9684
Malin Antonsson <malin@malinantonsson.com>
2017-06-10 20:35:03 +0100

    cleanup

--- javascript/actions/index.js
+++ javascript/actions/index.js
@@ -1,7 +1,4 @@
 import axios from 'axios';
-// Weather api key
-const API_KEY = '5d3dfc26f533ce64c9ac9b240327bd9a';
-//const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 const ROOT_URL = 'https://veggiedays-1b98e.firebaseio.com/recipes.json';
 
 export const FETCH_RECIPES = 'FETCH_RECIPES';
@@ -9,7 +6,7 @@ export const CREATE_RECIPE = 'CREATE_RECIPE';
 
 export function fetchRecipes(term) {
 	//finish the request url
-	const request = axios.get(`${ROOT_URL}?orderBy="date"&limitToLast=3`);
+	const request = axios.get(`${ROOT_URL}`);
 
   return {
     type: FETCH_RECIPES,
@@ -30,7 +27,6 @@ export function fetchLastThreeRecipes() {
 export function createRecipe(values, callback) {
 	const request = axios.post(`${ROOT_URL}`, values)
 		.then(() => callback());
-		//console.log(values);
 
 	return {
 		type: CREATE_RECIPE,
