07d379048b9bf9a74a5cc4f710c53305910bc502
Malin Antonsson <malin@malinantonsson.com>
2017-06-10 21:36:22 +0100

    get individual post by slug

--- javascript/reducers/reducer_recipes.js
+++ javascript/reducers/reducer_recipes.js
@@ -1,9 +1,13 @@
-import { FETCH_RECIPES } from '../actions/index';
+import _ from 'lodash';
+import { FETCH_RECIPES, FETCH_RECIPE } from '../actions/index';
 
 export default function(state = [], action) {
 	switch (action.type) {
 		case FETCH_RECIPES:
 			return [action.payload.data, ...state]; //do not manipulate the state (eg. state.push(), return a NEW array
+		case FETCH_RECIPE:
+			console.log(_.mapKeys(action.payload.data, 'slug'));
+			return _.mapKeys(action.payload.data, 'slug');
 	}
 	return state;
 }
