9bb036bdc1ee09e122d8f557206f762c44e833f0
Malin Antonsson <malin@malinantonsson.com>
2017-06-24 20:13:20 +0100

    stop from breaking if wrong recipe slug

--- javascript/reducers/reducer_recipes.js
+++ javascript/reducers/reducer_recipes.js
@@ -7,13 +7,17 @@ export default function(state = {}, action) {
 			return _.mapKeys(action.payload.data, 'slug');
 
 		case FETCH_RECIPE:
-			//get the firebaseKey from the object key
-			const firebaseKey = Object.getOwnPropertyNames(action.payload.data)[0];
-			//turn the object into an array so that we can get the first (and only) item
-			const recipe = _.map(action.payload.data)[0];
-			recipe.firebaseKey = firebaseKey;
-			 //do not manipulate the state (eg. state.push(), return a NEW array
-			return {...state, [recipe.slug]: recipe };
+			if(action.payload.data) {
+				//get the firebaseKey from the object key
+				const firebaseKey = Object.getOwnPropertyNames(action.payload.data)[0];
+				//turn the object into an array so that we can get the first (and only) item
+				const recipe = _.map(action.payload.data)[0];
+				recipe.firebaseKey = firebaseKey;
+				 //do not manipulate the state (eg. state.push(), return a NEW array
+				return {...state, [recipe.slug]: recipe };
+			}
+			return state;
+
 	}
 	return state;
 }
